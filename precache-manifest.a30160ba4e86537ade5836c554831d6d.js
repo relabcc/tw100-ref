66dc0295c3395d81b806dac5bb717407b407308e
peja <peja@relab.cc>
2019-11-26 18:53:06 +0800

    Updates

---
+++ precache-manifest.a30160ba4e86537ade5836c554831d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed3ab4d5478dd5be98e1191c5f694ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211c0c10204fc31833d4",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "211c0c10204fc31833d4",
+    "url": "/static/js/2.4f769d06.chunk.js"
+  },
+  {
+    "revision": "8bed9ae9a1902439f835",
+    "url": "/static/js/main.7d49e14d.chunk.js"
+  },
+  {
+    "revision": "ac2b2adca04be9f3df08",
+    "url": "/static/js/runtime-main.05ad3075.js"
+  }
+]);
